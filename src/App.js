99105a4914ad0107bf9f105013da74dbe552d1eb
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-07-15 19:45:01 +0530

    updated toggle mode for navBar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import TextSubmit from "./components/ThemeToggle";
 import ThemeToggle from "./components/ThemeToggle";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") setMode("dark");
+    else setMode("light");
+  };
   return (
     <>
-      <Navbar title="TextUtilities"></Navbar>
+      <Navbar
+        title="TextUtilities"
+        mode={mode}
+        toggleMode={toggleMode}
+      ></Navbar>
       <div id="textFormArea">
         <TextForm
           heading="Enter The Text Here"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import propTypes from "prop-types";
 export default function Navbar(props) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
         <div className="container-fluid" id="myNav">
           <a className="navbar-brand" href="/">
             {props.title}
@@ -35,56 +37,25 @@ export default function Navbar(props) {
                   About
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Link
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Link
-                </a>
-              </li>
             </ul>
             <form className="d-flex" role="search">
-              <div className="form-check form-switch">
+              <div
+                className={`form-check form-switch text-${
+                  props.mode === "dark" ? "light" : "dark"
+                }`}
+              >
                 <input
                   className="form-check-input"
                   type="checkbox"
                   role="switch"
                   id="flexSwitchCheckDefault"
+                  onClick={props.toggleMode}
                 />
                 <label
                   className="form-check-label"
                   for="flexSwitchCheckDefault"
                 >
-                  Default switch checkbox input
+                  Change Theme
                 </label>
               </div>
             </form>
