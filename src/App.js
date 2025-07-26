83e0885b849e172ba0632f8a8f45b11fc9e91a3b
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-07-26 23:27:07 +0530

    Implemented fully functional toggle mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import TextSubmit from "./components/About";
+//import TextSubmit from "./components/About";
 import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -23,23 +23,38 @@ function App() {
       setAlert(null);
     }, 2000);
   };
+  const removeBg = () => {
+    document.body.classList.remove("bg-primary");
+    document.body.classList.remove("bg-secondary");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-success");
+    document.body.classList.remove("bg-danger");
+  };
   const toggleMode = (cls) => {
     console.log(cls);
-    document.body.classList.add("bg-" + cls);
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#004080";
-      showAlert("Dark Mode Enabled", "success");
-      setInterval(() => {
-        document.title = "TextUtilities - Dark Mode";
+    removeBg();
+    try {
+      document.body.classList.add("bg-" + cls);
+      showAlert(`${cls} Mode Enabled`, "success");
+      setTimeout(() => {
+        document.title = `TextUtilities - ${cls} Mode`;
       }, 3000);
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light Mode Enabled", "success");
-      setInterval(() => {
-        document.title = "TextUtilities - Light Mode";
-      }, 1500);
+    } catch (err) {
+      if (mode === "light") {
+        setMode("dark");
+        document.body.style.backgroundColor = "#020f1cff";
+        showAlert("Dark Mode Enabled", "success");
+        setInterval(() => {
+          document.title = "TextUtilities - Dark Mode";
+        }, 3000);
+      } else {
+        setMode("light");
+        document.body.style.backgroundColor = "white";
+        showAlert("Light Mode Enabled", "success");
+        setInterval(() => {
+          document.title = "TextUtilities - Light Mode";
+        }, 3000);
+      }
     }
   };
   return (
